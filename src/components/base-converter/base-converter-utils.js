900799545e44be7e99ed09641a42d5b1c7cdcf88
Pozzi <eanderea1@gmail.com>
2025-04-15 00:55:30 -0300

    Swap Button + Layout

--- src/components/base-converter/base-converter-utils.js
+++ src/components/base-converter/base-converter-utils.js
@@ -9,21 +9,24 @@ for (let i = -32; i <= 32; i++) {
 export const BASE_OPTIONS = options;
 
 
-export function getBaseDisplayName(baseNum) {
+export function getBaseDisplayName(baseNum, hasParentheses = false) {
     const baseNames = {
-      32: "(Duotrigesimal)",
-      16: "(Hexadecimal)",
-      10: "(Decimal)",
-      8: "(Octal)",
-      3: "(Ternary)",
-      2: "(Binary)",
-      "-2": "(Negabinary)",
-      "-2": "(Negaternary)",
-      "-8": "(Negaoctal)",
-      "-10": "(Negadecimal)",
-      "-16": "(Negahexadecimal)",
-      "-32": "(Negaduotrigesimal)"
+      32: "Duotrigesimal",
+      16: "Hexadecimal",
+      10: "Decimal",
+      8: "Octal",
+      3: "Ternary",
+      2: "Binary",
+      "-2": "Negabinary",
+      "-3": "Negaternary",
+      "-8": "Negaoctal",
+      "-10": "Negadecimal",
+      "-16": "Negahexadecimal",
+      "-32": "Negaduotrigesimal"
     };
+    if (hasParentheses && baseNames[baseNum] ) {
+      return `(${baseNames[baseNum]})` || "";
+    }
     return baseNames[baseNum] || "";
   }
   
